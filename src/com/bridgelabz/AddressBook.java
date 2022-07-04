2a3909da4929a17700ee92a08d5fe88556b99683
nagarajnsg1995 <nagarajsbg023@gmail.com>
2022-07-04 19:07:16 +0530

    Added: New contact to the address book

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -2,12 +2,18 @@ package com.bridgelabz;
 
 import java.util.ArrayList;
 import java.util.Scanner;
-public class AddressBook {
-    static Scanner scanner = new Scanner(System.in);
-    static ArrayList<Contact> addressBook = new ArrayList<Contact>();
 
+public class AddressBook {
+
+    static Scanner scanner = new Scanner(System.in);
+    static ArrayList<Contact> addressBook = new ArrayList<>();
     public static void main(String[] args) {
-        System.out.println("Welcome to the Address Book Problem");
+        System.out.println("Welcome to the Address Book");
+        AddressBook addressBookList = new AddressBook();
+        addressBookList.addContactDetails();
+    }
+    public void addContactDetails() {
+        Contact details = new Contact();
         System.out.println("Enter a first name:");
         String firstName = scanner.next();
         System.out.println("Enter a last name:");
@@ -25,9 +31,11 @@ public class AddressBook {
         System.out.println("Enter a phone number:");
         long phoneNumber = scanner.nextLong();
 
+
+        addressBook.add(details);
         Contact details1 = new Contact(firstName, lastName, address, city, state, email, zip, phoneNumber);
         addressBook.add(details1);
         System.out.print(addressBook);
-        System.out.println("successfully created contacts");
+        System.out.println("successfully added person new contacts");
     }
 }
