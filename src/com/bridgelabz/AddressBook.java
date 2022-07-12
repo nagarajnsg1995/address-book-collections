e64968227f1733136343f88d7a071d7bcebd1f70
nagarajnsg1995 <nagarajsbg023@gmail.com>
2022-07-12 18:46:45 +0530

    Added: edit contact to address book

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -4,38 +4,118 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AddressBook {
-
     static Scanner scanner = new Scanner(System.in);
-    static ArrayList<Contact> addressBook = new ArrayList<>();
+    ArrayList<Contact> Book = new ArrayList<>();
+
     public static void main(String[] args) {
-        System.out.println("Welcome to the Address Book");
-        AddressBook addressBookList = new AddressBook();
-        addressBookList.addContactDetails();
+        int i = 0;
+
+        System.out.println("Welcome to Address Book Management System");
+
+        AddressBook details = new AddressBook();
+
+        while (i == 0) {
+
+            System.out.println("1.Add details.\n2.Edit details.");
+            int choose = scanner.nextInt();
+            switch (choose) {
+                case 1:
+                    details.AddDetails();
+                    break;
+                case 2:
+                    details.editDetails();
+                    break;
+                default:
+                    i = 1;
+                    System.out.println("Wrong option");
+                    details.output();
+                    break;
+            }
+        }
+
     }
-    public void addContactDetails() {
+
+    //   edit the details
+    public void AddDetails() {
+
+        Scanner scanner = new Scanner(System.in);
         Contact details = new Contact();
-        System.out.println("Enter a first name:");
-        String firstName = scanner.next();
-        System.out.println("Enter a last name:");
-        String lastName = scanner.next();
-        System.out.println("Enter a Address:");
-        String address = scanner.next();
-        System.out.println("Enter a City name:");
-        String city = scanner.next();
-        System.out.println("Enter a state:");
-        String state = scanner.next();
-        System.out.println("Enter a email:");
-        String email = scanner.next();
-        System.out.println("Enter a zip code:");
-        int zip = scanner.nextInt();
-        System.out.println("Enter a phone number:");
-        long phoneNumber = scanner.nextLong();
+        System.out.println("Enter the First Name ");
+        details.setFirstName(scanner.nextLine());
+        System.out.println("Enter the Last Name ");
+        details.setLastName(scanner.nextLine());
+        System.out.println("Enter the Address ");
+        details.setAddress(scanner.nextLine());
+        System.out.println("Enter the City ");
+        details.setCity(scanner.nextLine());
+        System.out.println("Enter the State ");
+        details.setState(scanner.nextLine());
+        System.out.println("Enter the Email ");
+        details.setEmail(scanner.nextLine());
+        System.out.println("Enter the Zip code ");
+        details.setZipCode(scanner.nextInt());
+        System.out.println("Enter the mobile number ");
+        details.setPhoneNumber(scanner.nextInt());
 
+        Book.add(details);
 
-        addressBook.add(details);
-        Contact details1 = new Contact(firstName, lastName, address, city, state, email, zip, phoneNumber);
-        addressBook.add(details1);
-        System.out.print(addressBook);
-        System.out.println("successfully added person new contacts");
+        System.out.println(Book);
+    }
+    public void editDetails() {
+
+        System.out.println("Confirm your first name to edit details: ");
+        String name = scanner.next();
+
+        for (int i = 0; i < Book.size(); i++) {
+            if (Book.get(i).getFirstName().equals(name)) {
+                System.out.println("Select form below to change: ");
+                System.out.println("\n1.First Name\n2.Last Name\n3.Address\n4.city\n5.State\n6.Email\n7.Zip\n8.Phone number");
+                int check = scanner.nextInt();
+
+                switch (check) {
+                    case 1:
+                        System.out.println("Enter first name");
+                        Book.get(i).setFirstName(scanner.next());
+                        break;
+                    case 2:
+                        System.out.println("Enter Last name");
+                        Book.get(i).setLastName(scanner.next());
+                        break;
+                    case 3:
+                        System.out.println("Enter address");
+                        Book.get(i).setAddress(scanner.next());
+                        break;
+                    case 4:
+                        System.out.println("Enter city");
+                        Book.get(i).setCity(scanner.next());
+                        break;
+                    case 5:
+                        System.out.println("Enter state");
+                        Book.get(i).setState(scanner.next());
+                        break;
+                    case 6:
+                        System.out.println("Enter email");
+                        Book.get(i).setEmail(scanner.next());
+                        break;
+                    case 7:
+                        System.out.println("Enter Zip");
+                        Book.get(i).setZipCode(scanner.nextInt());
+                        break;
+                    case 8:
+                        System.out.println("Enter phone number");
+                        Book.get(i).setPhoneNumber(scanner.nextInt());
+                        break;
+                }
+
+                System.out.println(Book);
+
+            } else System.out.println("Enter valid First name");
+        }
+
+    }
+
+    public void output() {
+
+        System.out.println(Book);
     }
 }
--- src/com/bridgelabz/Contact.java
+++ src/com/bridgelabz/Contact.java
@@ -7,36 +7,12 @@ public class Contact {
     private String address;
     private String city;
     private String state;
-    private int zip;
+    private long zipCode;
     private long phoneNumber;
     private String email;
 
-    Contact(String firstName, String lastName, String address, String city, String state, int zip, long phoneNumber, String email) {
-        this.setFirstName(firstName);
-        this.setLastName(lastName);
-        ;
-        this.setAddress(address);
-        this.setCity(city);
-        this.setState(state);
-        this.setZip(zip);
-        this.setPhoneNumber(phoneNumber);
-        this.setEmail(email);
-    }
+    public Contact() {
 
-    Contact() {
-
-    }
-
-    public Contact(String firstName2, String lastName2, String address2, String city2, String state2, String email2, int zip2, long phoneNumber2) {
-        this.firstName = firstName2;
-        this.lastName = lastName2;
-        this.address = address2;
-        this.city = city2;
-        this.state = state2;
-        this.zip = zip2;
-        this.phoneNumber = phoneNumber2;
-        this.email = email2;
-        // TODO Auto-generated constructor stub
     }
 
     public String getFirstName() {
@@ -79,12 +55,12 @@ public class Contact {
         this.state = state;
     }
 
-    public int getZip() {
-        return zip;
+    public long getZipCode() {
+        return zipCode;
     }
 
-    public void setZip(int zip) {
-        this.zip = zip;
+    public void setZipCode(long zipCode) {
+        this.zipCode = zipCode;
     }
 
     public long getPhoneNumber() {
@@ -105,7 +81,15 @@ public class Contact {
 
     @Override
     public String toString() {
-        return "contact [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", city=" + city + ", state=" + state + ", zip=" + zip + ", phoneNumber=" + phoneNumber + ", email=" + email + "]";
+        return "Contacts{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zipCode=" + zipCode +
+                ", phoneNumber=" + phoneNumber +
+                ", email='" + email + '\'' +
+                '}';
     }
 }
-
